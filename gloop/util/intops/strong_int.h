9458a26a18d4bec21eede13ffca63276a921b4c4
Eduardo Moreno <edomoreno@google.com>
2026-04-10 22:56:28 -0700

    Replace deprecated absl::enable_if_t and absl::conjunction.
    Replaces absl::enable_if_t and absl::conjunction with their std:: counterparts.
    
    PiperOrigin-RevId: 898054116

--- gloop/util/intops/strong_int.h
+++ gloop/util/intops/strong_int.h
@@ -317,7 +317,7 @@ class StrongInt {
   // Explicitly cast the raw value only if the underlying value is convertible
   // to T.
   template <typename T,
-            typename = absl::enable_if_t<absl::conjunction<
+            typename = std::enable_if_t<std::conjunction<
                 std::bool_constant<std::numeric_limits<T>::is_integer>,
                 std::is_convertible<ValueType, T>>::value>>
   constexpr explicit operator T() const {
