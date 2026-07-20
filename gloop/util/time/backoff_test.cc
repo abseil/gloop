9c80b1e5bc2cd1a1eb79e8e9f2ea28b22027aeda
Gloop Team <gloop-copybara-bot@google.com>
2026-07-20 12:30:10 -0700

    Internal cleanup
    PiperOrigin-RevId: 950989880

--- gloop/util/time/backoff_test.cc
+++ gloop/util/time/backoff_test.cc
@@ -20,8 +20,9 @@
 
 #include "gloop/util/time/backoff.h"
 
-#include "absl/base/attributes.h"
-#include "absl/base/macros.h"
+#include <array>
+#include <cstddef>
+
 #include "absl/time/time.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
@@ -34,9 +35,9 @@ constexpr double kBackoffBase = 1.3;
 MATCHER_P2(InRange, min, max, "") { return arg >= min && arg <= max; }
 
 TEST(ComputeBackoffTest, Simple) {
-  const absl::Duration ns = absl::Nanoseconds(1);
-  const absl::Duration min_d = 100 * ns;
-  const absl::Duration max_d = 1000 * ns;
+  constexpr absl::Duration ns = absl::Nanoseconds(1);
+  constexpr absl::Duration min_d = absl::Nanoseconds(100);
+  constexpr absl::Duration max_d = absl::Nanoseconds(1000);
 
   // Lower bound = (0.4 + 0.6 * 1.3^retries) * min_delay
   // Upper bound = (0.4 + 1.3^retries) * min_delay
@@ -56,14 +57,13 @@ TEST(ComputeBackoffTest, Simple) {
 }
 
 TEST(ComputeBackoffTest, AlwaysInMinMaxRange) {
-  const absl::Duration ns = absl::Nanoseconds(1);
-  const absl::Duration min_d = 100 * ns;
-  const absl::Duration max_d = 1000 * ns;
-  static const int kNumIterations = 10000;
+  constexpr absl::Duration min_d = absl::Nanoseconds(100);
+  constexpr absl::Duration max_d = absl::Nanoseconds(1000);
+  constexpr int kNumIterations = 10000;
 
   // kMaxRetries is large enough so 1.3^kMaxRetries is significantly
   // bigger than max_delay/min_delay
-  static const int kMaxRetries = 20;
+  constexpr int kMaxRetries = 20;
   for (int i = 0; i < kNumIterations; ++i) {
     for (int retries = 0; retries <= kMaxRetries; ++retries) {
       ASSERT_THAT(ComputeBackoff(min_d, max_d, kBackoffBase, retries),
@@ -73,13 +73,13 @@ TEST(ComputeBackoffTest, AlwaysInMinMaxRange) {
 }
 
 TEST(ComputeBackoffTest, MinIsNegative) {
-  const auto ns ABSL_ATTRIBUTE_UNUSED = absl::Nanoseconds(1);
-  const auto infinity ABSL_ATTRIBUTE_UNUSED = absl::InfiniteDuration();
+  [[maybe_unused]] constexpr auto ns = absl::Nanoseconds(1);
+  [[maybe_unused]] constexpr auto infinity = absl::InfiniteDuration();
 
 #ifdef NDEBUG
   // A negative value should be treated as a small positive value.
   const auto b = ComputeBackoff(-1 * ns, infinity, kBackoffBase, 0);
-  EXPECT_LE(absl::Nanoseconds(1), b);
+  EXPECT_GE(b, absl::Nanoseconds(1));
   EXPECT_LT(b, absl::Milliseconds(10));
 #elif GTEST_HAS_DEATH_TEST
   EXPECT_DEATH(
@@ -89,9 +89,9 @@ TEST(ComputeBackoffTest, MinIsNegative) {
 }
 
 TEST(ComputeBackoffTest, MinIsZero) {
-  const auto zero = absl::Duration();
-  const auto ns = absl::Nanoseconds(1);
-  const auto infinity = absl::InfiniteDuration();
+  constexpr auto zero = absl::Duration();
+  constexpr auto ns = absl::Nanoseconds(1);
+  constexpr auto infinity = absl::InfiniteDuration();
 
   EXPECT_GE(ComputeBackoff(zero, infinity, kBackoffBase, 0), 1 * ns);
   EXPECT_GE(ComputeBackoff(zero, infinity, kBackoffBase, 1), 1.18 * ns);
@@ -99,11 +99,11 @@ TEST(ComputeBackoffTest, MinIsZero) {
 }
 
 TEST(ComputeBackoffTest, MaxIsLessThanMin) {
-  const auto ns ABSL_ATTRIBUTE_UNUSED = absl::Nanoseconds(1);
+  [[maybe_unused]] constexpr auto ns = absl::Nanoseconds(1);
 
 #ifdef NDEBUG
   // A smaller value should be treated as equal to the min value.
-  EXPECT_EQ(100 * ns, ComputeBackoff(100 * ns, 90 * ns, kBackoffBase, 0));
+  EXPECT_EQ(ComputeBackoff(100 * ns, 90 * ns, kBackoffBase, 0), 100 * ns);
 #elif GTEST_HAS_DEATH_TEST
   EXPECT_DEATH(
       { (void)ComputeBackoff(100 * ns, 90 * ns, kBackoffBase, 0); },
@@ -112,13 +112,13 @@ TEST(ComputeBackoffTest, MaxIsLessThanMin) {
 }
 
 TEST(ComputeBackoffTest, MaxEqualsMin) {
-  const auto ns = absl::Nanoseconds(1);
-  EXPECT_EQ(100 * ns, ComputeBackoff(100 * ns, 100 * ns, kBackoffBase, 0));
+  constexpr auto ns = absl::Nanoseconds(1);
+  EXPECT_EQ(ComputeBackoff(100 * ns, 100 * ns, kBackoffBase, 0), 100 * ns);
 }
 
 TEST(ComputeBackoffTest, PreviousRetriesIsNegative) {
-  const auto ns ABSL_ATTRIBUTE_UNUSED = absl::Nanoseconds(1);
-  const auto infinity ABSL_ATTRIBUTE_UNUSED = absl::InfiniteDuration();
+  [[maybe_unused]] constexpr auto ns = absl::Nanoseconds(1);
+  [[maybe_unused]] constexpr auto infinity = absl::InfiniteDuration();
 
 #ifdef NDEBUG
   // A negative value should be treated as zero.
@@ -131,8 +131,8 @@ TEST(ComputeBackoffTest, PreviousRetriesIsNegative) {
 }
 
 TEST(ComputeBackoffTest, BackoffBaseEqualsToOne) {
-  const auto ns ABSL_ATTRIBUTE_UNUSED = absl::Nanoseconds(1);
-  const auto infinity ABSL_ATTRIBUTE_UNUSED = absl::InfiniteDuration();
+  [[maybe_unused]] constexpr auto ns = absl::Nanoseconds(1);
+  [[maybe_unused]] constexpr auto infinity = absl::InfiniteDuration();
 
 #ifdef NDEBUG
   // A backoff base <= 1.0 should be treated as 1.3.
@@ -148,8 +148,8 @@ TEST(ComputeBackoffTest, BackoffBaseEqualsToOne) {
 }
 
 TEST(ComputeBackoffTest, BackoffBaseIsLessThanOne) {
-  const auto ns ABSL_ATTRIBUTE_UNUSED = absl::Nanoseconds(1);
-  const auto infinity ABSL_ATTRIBUTE_UNUSED = absl::InfiniteDuration();
+  [[maybe_unused]] constexpr auto ns = absl::Nanoseconds(1);
+  [[maybe_unused]] constexpr auto infinity = absl::InfiniteDuration();
 
 #ifdef NDEBUG
   // A backoff base <= 1.0 should be treated as 1.3.
@@ -166,9 +166,9 @@ TEST(ComputeBackoffTest, BackoffBaseIsLessThanOne) {
 
 TEST(ComputeBackoffTest, SmallDurations) {
   // Make sure we do actually exponentially back off for low minimum delays.
-  EXPECT_LT(absl::Nanoseconds(1),
-            ComputeBackoff(absl::Nanoseconds(1), absl::Nanoseconds(1000),
-                           kBackoffBase, 10));
+  EXPECT_GT(ComputeBackoff(absl::Nanoseconds(1), absl::Nanoseconds(1000),
+                           kBackoffBase, 10),
+            absl::Nanoseconds(1));
   EXPECT_LT(ComputeBackoff(absl::Nanoseconds(1), absl::Nanoseconds(1000),
                            kBackoffBase, 10),
             ComputeBackoff(absl::Nanoseconds(1), absl::Nanoseconds(1000),
@@ -180,38 +180,35 @@ TEST(ComputeBackoffTest, ManyRetries) {
   const auto backoff = ComputeBackoff(
       absl::Nanoseconds(1), absl::InfiniteDuration(), kBackoffBase, 1 << 30);
 
-  EXPECT_LT(absl::Nanoseconds(1), backoff);
+  EXPECT_GT(backoff, absl::Nanoseconds(1));
   EXPECT_LE(backoff, absl::InfiniteDuration());
 }
 
 TEST(ComputeBackoffTest, Distribution) {
   // Run ComputeBackoff() a bunch of times and keep track of how many
   // times each value occurred.
-  int counts[1001];
-  for (int i = 0; i < ABSL_ARRAYSIZE(counts); i++) {
-    counts[i] = 0;
-  }
+  std::array<int, 1001> counts = {};
   for (int i = 0; i < 100000; i++) {
     const auto d = ComputeBackoff(absl::Nanoseconds(10),
                                   absl::Nanoseconds(1000), kBackoffBase, 100);
 
     const auto ns = d / absl::Nanoseconds(1);
-    ASSERT_GT(ABSL_ARRAYSIZE(counts), ns);
+    ASSERT_GT(counts.size(), ns);
     counts[ns]++;
   }
   int too_small = 0;
   int too_large = 0;
-  for (int i = 0; i < ABSL_ARRAYSIZE(counts); i++) {
+  for (size_t i = 0; i < counts.size(); i++) {
     if (i < 600) {
-      EXPECT_EQ(0, counts[i]) << i;
+      EXPECT_EQ(counts[i], 0) << i;
     } else {
       // The 400 acceptable slots must each have ~ 100000/400 = 250.
       if (counts[i] < 200) too_small++;
       if (counts[i] > 300) too_large++;
     }
   }
-  EXPECT_GE(10, too_small);
-  EXPECT_GE(10, too_large);
+  EXPECT_LE(too_small, 10);
+  EXPECT_LE(too_large, 10);
 }
 
 }  // namespace
