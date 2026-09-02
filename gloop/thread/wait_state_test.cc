e1729247704dc83fce75a6c5b99614c5ec6df368
Gloop Team <gloop-copybara-bot@google.com>
2026-09-02 07:06:38 -0700

    Delete dynamic_threadpool.
    This class has been deprecated since it was introduced to thread/ in 2008. All callers have been removed.
    
    PiperOrigin-RevId: 975110500

--- gloop/thread/wait_state_test.cc
+++ gloop/thread/wait_state_test.cc
@@ -41,7 +41,6 @@
 #include "absl/time/time.h"
 #include "gloop/base/thread-identity.h"
 #include "gloop/base/walltime.h"
-#include "gloop/thread/dynamic_threadpool.h"
 #include "gloop/thread/fiber/channel.h"
 #include "gloop/thread/fiber/fiber-options.h"
 #include "gloop/thread/fiber/fiber.h"
@@ -278,18 +277,6 @@ std::string RunOnThreadPool(absl::AnyInvocable<void() &&> f) {
   return kName;
 }
 
-std::string RunOnDynamicThreadPool(absl::AnyInvocable<void() &&> f) {
-  const std::string kName = "active-worker-test-dynamic-threadpool";
-  static absl::NoDestructor<std::unique_ptr<DynamicThreadPool>> pool([&]() {
-    auto pool = std::make_unique<DynamicThreadPool>(
-        kName, DynamicThreadPool::Options());
-
-    return pool;
-  }());
-  (*pool)->Schedule(std::move(f));
-  return kName;
-}
-
 std::string RunOnPeriodicClosure(absl::AnyInvocable<void() &&> f) {
   static constexpr absl::string_view kName =
       "active-worker-test-periodic-closure";
@@ -348,8 +335,7 @@ std::string RunOnFiber(absl::AnyInvocable<void() &&> f) {
 }  // namespace
 
 INSTANTIATE_TEST_SUITE_P(ExecutorImpls, ActiveWorkerTest,
-                         ::testing::Values(RunOnDynamicThreadPool,
-                                           RunOnPeriodicClosure, RunOnFiber,
+                         ::testing::Values(RunOnPeriodicClosure, RunOnFiber,
                                            RunOnThreadPool, RunOnTimedCall));
 
 }  // namespace thread
