e9b8f0415b341d4229ddb3c4c8a725661b1ee80c
Martijn Vels <mvels@google.com>
2026-09-15 18:37:38 -0700

    Restore automatic capture of source location for WithLifetimeBoundContext
    PiperOrigin-RevId: 982174104

---
+++ gloop/base/context_ffi.h
@@ -0,0 +1,43 @@
+// Copyright 2026 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Removing the following header is prohibited as it can introduce undefined
+// behavior.
+// clang-format off
+#include "gloop/enforce_gloop_support.h"
+// clang-format on
+
+#ifndef THIRD_PARTY_GLOOP_BASE_CONTEXT_FFI_H_
+#define THIRD_PARTY_GLOOP_BASE_CONTEXT_FFI_H_
+
+#include "base/with_lifetime_bound_context.h"
+#include "gloop/base/context.h"
+
+namespace base::internal::context_rs {
+
+// `WithLifetimeBoundContextForRust` is a proxy class to bypass the problem with
+// Crubit not being able to properly pass defaulted arguments for constructors.
+// We really want to capture default source locations for C++ and only disable
+// the automatic source location capture for Crubit where it does not work.
+class WithLifetimeBoundContextForRust {
+ public:
+  explicit WithLifetimeBoundContextForRust(Context& context) : with_(context) {}
+
+ private:
+  ::base::subtle::WithLifetimeBoundContext with_;
+};
+
+}  // namespace base::internal::context_rs
+
+#endif  // THIRD_PARTY_GLOOP_BASE_CONTEXT_FFI_H_
