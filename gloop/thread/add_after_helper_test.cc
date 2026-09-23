5f93df3f17802e93f838e6f40a58813eee862b11
Gloop Team <gloop-copybara-bot@google.com>
2026-09-22 22:53:43 -0700

    Internal cleanup
    PiperOrigin-RevId: 986496632

--- gloop/thread/add_after_helper_test.cc
+++ gloop/thread/add_after_helper_test.cc
@@ -28,7 +28,6 @@
 #include <vector>
 
 #include "absl/functional/any_invocable.h"
-#include "absl/log/check.h"
 #include "absl/synchronization/notification.h"
 #include "absl/time/clock.h"
 #include "absl/time/clock_interface.h"
